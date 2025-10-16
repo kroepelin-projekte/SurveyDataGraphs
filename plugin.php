f325f4ece422ed25e15b0bd16e2776a808e51244
Ioanna Mitroulaki <ioanna.mitroulaki@kroepelin-projekte.de>
2025-10-16 12:50:02 +0200

    Update plugin version

--- plugin.php
+++ plugin.php
@@ -1,7 +1,7 @@
 <?php
 $id = "sdg";
-$version = "3.0.0";
-$ilias_min_version = "10.0";
-$ilias_max_version = "10.999";
+$version = "4.0.0";
+$ilias_min_version = "11.0";
+$ilias_max_version = "11.999";
 $responsible = "kpg";
 $responsible_mail = "support@kroepelin-projekte.de";
